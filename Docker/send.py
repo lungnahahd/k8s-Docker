1dc79cf4acb3cf403cf81614fd96ef3ca1fad0e5
Lungnaha <67555400+lungnahahd@users.noreply.github.com>
2022-05-27 23:53:33 +0900

    Feat : "데이터 활용"
    json 결과 활용 기능 추가

--- Docker/send.py
+++ Docker/send.py
@@ -1,11 +1,23 @@
 import requests
+import os
+import json
 
 def send_data(url):
-    files = {
-        'file':open('C:\\Users\\ASUS\\Desktop\\hi\\image2.jpg', 'rb')
-    }
-    res = requests.post(url,files=files)
-    return res.text
+    path_dir = '/root/pi'
+    file_list = os.listdir(path_dir)
+    for name in file_list:
+        files = {
+        #'file':open('C:\\Users\\ASUS\\Desktop\\hi\\hufs.jpg', 'rb')
+            'file':open('/root/pi/' + name, 'rb')
+        }
+        res = requests.post(url,files=files)
+        #name = res['name']
+        res_json = res.json()
+        if res_json['answer'] == "Detect":
+            os.system("mv /root/pi/" + name + " /root/web" )
+        else:
+            os.system("rm -f /root/pi/"+ name)
+    return "Done"
 
-url = "http://192.168.56.30:5000/predict"
+url = "http://192.168.56.31:32735/predict"
 print(send_data(url))
